f4b4f0043dd73e75915d23e37bda197d1166feb1
Mark Smith <smith.h.mark@gmail.com>
2022-03-08 19:27:16 -0500

    adding logging

--- src/kademlia/mod.rs
+++ src/kademlia/mod.rs
@@ -3,6 +3,7 @@
 
 //use std::cmp;
 
+use log::{debug};
 use std::collections::HashMap;
 use std::net::{IpAddr, Ipv4Addr};
 
@@ -107,29 +108,29 @@ impl RTable for VectorRoutingTable {
 
     fn insert(&mut self, other: NodeDescription) {
         let distance = iddiff(&self.id, &other.id);
-        println!("\tdistance:{}", distance);
+        debug!("\tdistance:{}", distance);
         let bucket = bucket_id(&self.id, &other.id, self.key_space);
-        println!("\tbucket:{}", bucket);
+        debug!("\tbucket:{}", bucket);
         //let insert_at = if distance >= self.hoods.len() as NodeID {
         let initial_len = self.hoods.len();
         let last_bucket = initial_len - 1;
         let insert_at = if bucket >= self.hoods.len() {
-            println!(
+            debug!(
                 "\tinserting into a bucket {} that doesn't exist yet",
                 bucket
             );
             last_bucket
         } else {
-            println!("\tdistance hits 'near' neighborhood");
+            debug!("\tdistance hits 'near' neighborhood");
             distance as usize
         };
         if self.hoods[insert_at].len() < self.kay() {
-            println!("\tplenty of room for the desc");
+            debug!("\tplenty of room for the desc");
             self.hoods[insert_at].push(other);
         } else {
-            println!("\tNo room");
+            debug!("\tNo room");
             if insert_at == last_bucket {
-                println!("\tpartitioning furthest hood:{:?}", self.hoods[insert_at]);
+                debug!("\tpartitioning furthest hood:{:?}", self.hoods[insert_at]);
                 let mut new_buckets: HashMap<usize, Neighborhood> = HashMap::new();
                 for (k, v) in self.hoods[insert_at]
                     .iter()
@@ -138,31 +139,31 @@ impl RTable for VectorRoutingTable {
                     new_buckets.entry(k).or_insert_with(Vec::new).push(v);
                 }
                 //let split_into = new_buckets.len();
-                println!("\t\tsplit bucket into {}", new_buckets.len());
+                debug!("\t\tsplit bucket into {}", new_buckets.len());
                 let closest = new_buckets.keys().max();
                 let furthest = new_buckets.keys().min();
-                println!("\t\tclosest bucket {}", closest.unwrap());
-                println!("\t\tfurthest bucket {}", furthest.unwrap());
-                println!("\t\tdealing with orig buck:{}", last_bucket);
+                debug!("\t\tclosest bucket {}", closest.unwrap());
+                debug!("\t\tfurthest bucket {}", furthest.unwrap());
+                debug!("\t\tdealing with orig buck:{}", last_bucket);
                 match new_buckets.get(&last_bucket) {
                     Some(content) => self.hoods[last_bucket] = content.to_vec(),
                     None => self.hoods[last_bucket] = Vec::with_capacity(self.kay()),
                 }
                 for buck_id in (initial_len)..=*closest.expect("should have split into buckets") {
-                    println!("\t\tdealing with buck:{}", buck_id);
+                    debug!("\t\tdealing with buck:{}", buck_id);
                     match new_buckets.get(&buck_id) {
                         Some(content) => {
-                            println!("\t\t\thad data");
+                            debug!("\t\t\thad data");
                             self.hoods.push(content.to_vec())
                         }
                         None => self.hoods.push(Vec::with_capacity(self.kay())),
                     }
                 }
-                //println!("\n at_dist hood:{:?}", at_dist);
-                //println!("\n further hood:{:?}", farther);
+                //debug!("\n at_dist hood:{:?}", at_dist);
+                //debug!("\n further hood:{:?}", farther);
                 self.hoods[bucket].push(other);
             } else {
-                println!("\tNo room, take a hike");
+                debug!("\tNo room, take a hike");
                 // possibly maintain pool of replacements
             };
         };
@@ -173,6 +174,10 @@ impl RTable for VectorRoutingTable {
 mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
+
+    fn setup() {
+        let _ = env_logger::builder().is_test(true).try_init();
+    }
 
     #[test]
     fn test_table_initialization() {
@@ -200,6 +205,7 @@ mod tests {
 
     #[test]
     fn test_table_insert() {
+        setup();
         let key_space = 4;
         let kay = 2;
         let mut table = VectorRoutingTable::new(0, key_space, kay);
@@ -221,12 +227,11 @@ mod tests {
         assert_eq!(2, table.hoods[0].len());
         println!("third insert");
         table.insert(NodeDescription::_dummy(2));
-        println!("sizes:{:?}", table._pop_by_hood());
+        println!("sizes:{:#?}", table._pop_by_hood());
         assert_eq!(3, table.hoods.len());
         assert_eq!(3, table.population());
         assert_eq!(0, table.hoods[0].len());
         assert_eq!(1, table.hoods[1].len());
-        assert_eq!(2, table.hoods[2].len());
     }
 
     #[test]
